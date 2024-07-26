821edb8a0522bdc98114e9ac6ae149f0b8fd02e8
ShaileshIshere <markvaldoz009@gmail.com>
2024-07-26 20:55:17 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const rootRouter = require("./routes/index");
+const cors = require("cors");
 const PORT = 3000;
 
 const app = express();
+
+router.use(cors(
+    {
+        origin: ["https://beam-game-engine-client.vercel.app"],
+        methods: ["POST", "GET", "PUT", "PATCH"],
+        credentials: true
+    }
+));
 
 app.use(express.json());
 app.use("/api", rootRouter);
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,17 +2,9 @@ const express = require("express");
 const userRouter = require("./user");
 const accountRouter = require("./account");
 const gamesRouter = require("./games");
-const cors = require("cors");
 
 const router = express.Router();
 
-router.use(cors(
-    {
-        origin: ["https://beam-game-engine-client.vercel.app"],
-        methods: ["POST", "GET", "PUT", "PATCH"],
-        credentials: true
-    }
-));
 
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
